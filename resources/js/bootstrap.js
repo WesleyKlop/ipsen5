6de5982d69209e68e4f406304531142b1b9f69fc
Wesley Klop <wesley19097@gmail.com>
2019-05-10 19:35:37 +0200

    Add example component/style and empty directories for parts

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,14 +5,12 @@
  */
 import Axios from 'axios'
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+Axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 const token = document.head.querySelector('meta[name="csrf-token"]')
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
+    Axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token')
 }
-
-window.axios = Axios
