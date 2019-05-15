9ae06a905e2e9f7d61fbdb06f36b5c9e34803da7
Sergi Philipsen <philipsen.sergi@gmail.com>
2019-05-15 16:38:54 +0200

    Add login page for voter and add card component with styling

--- resources/js/containers/App.js
+++ resources/js/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import VoterMainPage from '../routes/VoterMainPage'
 import InfoPage from '../routes/InfoPage'
 import PropositionPage from '../routes/PropositionPage'
@@ -11,6 +11,7 @@ import Header from '../components/Header'
 const App = () => (
     <>
         <Header/>
+        <div style={{flex: "1"}} />
         <BrowserRouter>
             <Switch>
                 <Route path="/info" component={InfoPage}/>
@@ -22,6 +23,7 @@ const App = () => (
                 <Route path="/" exact component={VoterMainPage}/>
             </Switch>
         </BrowserRouter>
+        <div style={{flex: "2"}} />
     </>
 )
 
--- resources/js/routes/VoterMainPage.js
+++ resources/js/routes/VoterMainPage.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import Card from "../components/card/Card";
+import CardHeader from "../components/card/CardHeader";
+import CardBody from "../components/card/CardBody";
+import CardButtons from "../components/card/CardButtons";
+import Button from "../components/card/Button";
 
-const VoterMainPage = () => (
-    <div className="login voter">
-        <div className="header">
-            Login met code
-        </div>
-    </div>
-)
+class VoterMainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loginCode: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardHeader className="yellow">
+                    Login met code
+                </CardHeader>
+                <CardBody>
+                    <input className="input" type="tel" pattern="[0-9]{6}" required autoComplete="off"
+                           placeholder="Je code"
+                           maxLength="6" value={this.state.loginCode} onChange={this.handleChange}/>
+                    <CardButtons>
+                        <Button onClick={this.loginVoter} className="block">Deelnemen</Button>
+                    </CardButtons>
+                </CardBody>
+            </Card>
+        )
+    }
+
+    handleChange = (event) => (
+        this.setState({loginCode: event.target.value})
+    )
+
+    loginVoter = () => (
+        console.log(this.state.loginCode)
+    )
+}
 
 export default VoterMainPage
