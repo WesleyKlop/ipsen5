75a6e7e24e763822307c0d1672a65da79ef2910d
Sergi Philipsen <philipsen.sergi@gmail.com>
2019-05-17 15:58:36 +0200

    Add 404 page

--- resources/js/containers/App.js
+++ resources/js/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import VoterMainPage from '../routes/VoterMainPage'
 import InfoPage from '../routes/InfoPage'
 import PropositionPage from '../routes/PropositionPage'
@@ -7,6 +7,7 @@ import FeedbackPage from '../routes/FeedbackPage'
 import VoterResultsPage from '../routes/VoterResultsPage'
 import EmailPage from '../routes/EmailPage'
 import Header from '../components/Header'
+import PageNotFoundPage from "../routes/PageNotFoundPage";
 
 const App = () => (
     <>
@@ -20,6 +21,7 @@ const App = () => (
                 <Route path="/results" component={VoterResultsPage}/>
                 <Route path="/email" component={EmailPage}/>
                 <Route path="/" exact component={VoterMainPage}/>
+                <Route component={PageNotFoundPage} status={404}/>
             </Switch>
         </BrowserRouter>
     </>
