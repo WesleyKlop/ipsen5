10761b92418690c235b11af3f11e0db48bacee1f
Dodo <kasper199914@gmail.com>
2019-05-17 16:43:56 +0200

    A user can only answer a proposition once

--- app/Eloquent/AnswersPropositions.php
+++ app/Eloquent/AnswersPropositions.php
@@ -34,10 +34,18 @@ trait AnswersPropositions
 
     public function submitAnswers(Collection $answers)
     {
-        $this->answers()->saveMany($answers->map(function(array $answer) {
+        $this->answers()->saveMany($answers->map(function (array $answer) {
+            if (Answer
+                ::where('proposition_id', $answer['proposition_id'])
+                ->where('survey_id', $this->survey->id)
+                ->where('user_id', $this->user_id)
+                ->exists()) {
+                throw new \Exception("Dit mag niet");
+            }
+
             return Answer::make([
                 'id' => Uuid::uuid4(),
-                'proposition_id'=> $answer['proposition_id'],
+                'proposition_id' => $answer['proposition_id'],
                 'survey_id' => $this->survey->id,
                 'answer' => $answer['answer']
             ]);
