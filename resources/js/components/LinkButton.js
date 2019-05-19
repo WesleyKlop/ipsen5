1d620a40eac7a4807572ef19b4839cc8075445fe
Wesley Klop <wesley19097@gmail.com>
2019-05-19 10:36:00 +0200

    Fix code formatting

--- resources/js/components/LinkButton.js
+++ resources/js/components/LinkButton.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 import Button from './Button'
 
 const LinkButton = ({ to, buttonClassName, linkClassName, children, ...props }) => (
-  <Link to={to} className={linkClassName} {...props}>
-    <Button className={buttonClassName}>{children}</Button>
-  </Link>
+    <Link to={to} className={linkClassName} {...props}>
+        <Button className={buttonClassName}>{children}</Button>
+    </Link>
 )
 
 export default LinkButton
