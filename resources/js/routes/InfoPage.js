2fd2611d061f62fa7c0723c668f3bd1ca4a28aab
Wesley Klop <wesley19097@gmail.com>
2019-05-19 10:40:13 +0200

    Implement LinkButton on Info page

--- resources/js/routes/InfoPage.js
+++ resources/js/routes/InfoPage.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import CardHeader from "../components/card/CardHeader";
-import CardBody from "../components/card/CardBody";
-import CardButtons from "../components/card/CardButtons";
-import Button from "../components/card/Button";
-import Card from "../components/card/Card";
-import {Link} from 'react-router-dom'
+import CardHeader from '../components/card/CardHeader'
+import CardBody from '../components/card/CardBody'
+import CardButtons from '../components/card/CardButtons'
+import Card from '../components/card/Card'
+import LinkButton from '../components/LinkButton'
 
 
 class InfoPage extends React.Component {
@@ -12,23 +11,25 @@ class InfoPage extends React.Component {
     render() {
         return (
             <>
-                <div style={{flex: '1'}}/>
+                <div style={{ flex: '1' }}/>
                 <Card>
                     <CardHeader>
                         Uitleg
                     </CardHeader>
                     <CardBody>
-                        <h2>Over de pijling</h2>
+                        <h2>Over de peiling</h2>
                         <p>U zult een aantal stellingen krijgen die met eens of oneens te beantwoorden zijn.
                             Zodra u een optie kiest gaat u door naar de volgende stelling. Veel succes!</p>
                         <CardButtons>
-                            <Link className="block" to={'/proposition/1'}>
-                                <Button className="block">Begin</Button>
-                            </Link>
+                            <LinkButton
+                                buttonClassName="block"
+                                linkClassName="block"
+                                to="/proposition"
+                            >Begin</LinkButton>
                         </CardButtons>
                     </CardBody>
                 </Card>
-                <div style={{flex: '2'}}/>
+                <div style={{ flex: '2' }}/>
             </>
         )
     }
