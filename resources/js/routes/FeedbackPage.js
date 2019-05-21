03b843e6340484e4d9c0533117ab719808a54535
Tom <dejongtom01@gmail.com>
2019-05-21 13:07:54 +0200

    missing the actual slider

--- resources/js/routes/FeedbackPage.js
+++ resources/js/routes/FeedbackPage.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import Card from "../components/card/Card";
+import CardHeader from "../components/card/CardHeader";
+import CardBody from "../components/card/CardBody";
+import CardButtons from "../components/card/CardButtons";
+import {Link} from "react-router-dom";
+import Button from "../components/card/Button";
 
-const FeedbackPage = () => (
-    <div/>
-)
+class FeedbackPage extends React.Component {
 
+
+    render() {
+        return (
+            <>
+                <div style={{flex: '1'}}/>
+                <Card>
+                    <CardHeader>
+                        Klaar!!!
+                    </CardHeader>
+                    <CardBody>
+                        <h2>Over de pijling</h2>
+                        <p>U bent klaar met de peiling! Geef de peiling een beoordeling om door te gaan</p>
+                        <slider/>
+                        <CardButtons>
+                            <Button className="block">Resultaten bekijken</Button>
+                        </CardButtons>
+                    </CardBody>
+                </Card>
+                <div style={{flex: '2'}}/>
+            </>
+        )
+    }
+    
+}
 export default FeedbackPage
