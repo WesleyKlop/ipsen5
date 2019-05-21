82054e213d0ac0b0dd3930b2eebf527111f219cd
wnicolaas <wietsenicolaas@gmail.com>
2019-05-21 11:20:02 +0200

    Admin login improved // Added more mdc dependencies

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -4,5 +4,5 @@ import {MDCTextField} from "@material/textfield/component";
 autoInit();
 
 //Instantiate textfields so the animation works
-const Usernamefield = new MDCTextField(document.querySelector('.username-mdc'));
-const Passwordfield = new MDCTextField(document.querySelector('.password-mdc'));
+const Usernamefield = new MDCTextField(document.querySelector('.mdc-username'));
+const Passwordfield = new MDCTextField(document.querySelector('.mdc-password'));
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,27 +14,37 @@
     <link rel="manifest" href="/manifest.json">
 </head>
 <body>
+    <div class="content-wrapper">
+        <img src="/images/logo_shadow.png" class="logo">
+        <div class="mdc-card">
+            <form action="/admin/login" method="post" class="login-container">
+                @csrf
+                <div class="mdc-text-field mdc-username">
+                    <input type="text" name="username" autocomplete="username" class="mdc-text-field__input"/>
+                    <div class="mdc-line-ripple"></div>
+                    <label class="mdc-floating-label inputlabel">Gebruikersnaam</label>
+                </div>
 
-<div class="mdc-card logincard">
-    <form action="/admin/login" method="post" class="flex-container">
-        @csrf
-        <div class="mdc-text-field username-mdc">
-            <input type="text" name="username" autocomplete="username" class="mdc-text-field__input"/>
-            <label class="mdc-floating-label inputlabel">Gebruikersnaam</label>
-            <div class="mdc-line-ripple"></div>
+                <div class="mdc-text-field mdc-password">
+                    <input type="password" name="password" autocomplete="current-password" class="mdc-text-field__input"/>
+                    <label class="mdc-floating-label inputlabel">Wachtwoord</label>
+                    <div class="mdc-line-ripple"></div>
+                </div>
+
+                <label class="mdc-typography--caption caption">Minimaal 8 karakters</label>
+
+
+
+                <button class="mdc-button mdc-button--raised">
+                    <span class="mdc-button__label">Inloggen</span>
+                </button>
+
+                <button class="mdc-button">
+                    <span class="mdc-button__label">Registreren</span>
+                </button>
+                {{--<input type="submit"/>--}}
+            </form>
         </div>
-
-        <div class="mdc-text-field password-mdc">
-            <input type="password" name="password" autocomplete="current-password" class="mdc-text-field__input"/>
-            <label class="mdc-floating-label inputlabel">Wachtwoord</label>
-            <div class="mdc-line-ripple"></div>
-        </div>
-
-        <button class="mdc-button">
-            <span class="mdc-button__label">Button</span>
-        </button>
-        {{--<input type="submit"/>--}}
-    </form>
-</div>
+    </div>
 </body>
 </html>
