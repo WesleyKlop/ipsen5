f2c8cb2a7ae68ceee24bbf61a87e251a495fd51a
Tom <dejongtom01@gmail.com>
2019-05-21 16:45:00 +0200

    a working slider with a bit of css.

--- resources/js/components/Slider.js
+++ resources/js/components/Slider.js
@@ -1,25 +1,33 @@
 import React from "react";
 
-const Slider = ({ value }) => {
 
-
-    const onValueChange = e => {
-        // onChange(e)
-        value = e.currentTarget.value,
-        console.log(e.currentTarget.value)
+class Slider extends React.Component {
+    state = {
+        value: '50'
     }
 
-    return (
-        <div class="slider">
-            <input
-                class="slider"
-                type="range"
-                min="0" max="100"
-                value={value}
-                onChange={onValueChange}
-                step="1"/>
-        </div>
-    )
+    onValueChange = e => {
+        console.log(e.currentTarget.value);
+        this.setState({value: e.currentTarget.value})
+        // this.setState('100')
+    }
+
+
+    render() {
+        return (
+            <div className="slider">
+                <input
+                    className="slider"
+                    type="range"
+                    min="0" max="100"
+                    value={this.state.value}
+                    // onChange={this.onValueChange}
+                    // onChange={value => this.setState({value}, this.onValueChange)}
+                    onChange={this.onValueChange}
+                    step="1"/>
+            </div>
+        )
+    }
 }
 
 export default Slider
