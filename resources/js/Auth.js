50d1f0078d9e28be7780754fc48ee8ce2656ec93
Sergi Philipsen <philipsen.sergi@gmail.com>
2019-06-06 16:13:21 +0200

    Fix is authorized check

--- resources/js/Auth.js
+++ resources/js/Auth.js
@@ -3,7 +3,7 @@ import jwtDecode from 'jwt-decode'
 const Auth = {
     getJWT: () => sessionStorage.getItem('jwt'),
     isAuthenticated: () => Auth.getJWT() !== null,
-    isAuthorized: (type) => jwtDecode(Auth.getJWT()).type === type,
+    isAuthorized: (type) => jwtDecode(Auth.getJWT()).sub.type === type,
     authenticate: (jwt) => sessionStorage.setItem('jwt', jwt['jwt']),
     signout: () => sessionStorage.removeItem('jwt')
 }
