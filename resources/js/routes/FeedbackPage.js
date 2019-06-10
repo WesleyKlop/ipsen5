27588b73327ffc496ff5f8fdaf88bc033983b56c
Wesley Klop <wesley19097@gmail.com>
2019-06-10 18:15:04 +0200

    Add feedback page base from feature/feadback branch

--- resources/js/routes/FeedbackPage.js
+++ resources/js/routes/FeedbackPage.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import Spacer from '../components/Spacer'
+import CardHeader from '../components/card/CardHeader'
+import CardBody from '../components/card/CardBody'
+import CardButtons from '../components/card/CardButtons'
+import LinkButton from '../components/LinkButton'
+import Card from '../components/card/Card'
 
-const FeedbackPage = () => <div />
+const FeedbackPage = () => (
+  <>
+    <Spacer/>
+    <Card>
+      <CardHeader>
+        Klaar!
+      </CardHeader>
+      <CardBody>
+        <p>U bent klaar met de peiling! Geef de peiling en beoordeling om uw resultaat te zien.</p>
+        <CardButtons>
+          <LinkButton block to={'/results'}>Resultaten bekijken</LinkButton>
+        </CardButtons>
+      </CardBody>
+    </Card>
+    <Spacer size={2}/>
+  </>
+)
 
 export default FeedbackPage
