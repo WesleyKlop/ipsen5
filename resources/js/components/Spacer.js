f12eb444ffe0b7524528bdd77d12bcb9396945c5
Wesley Klop <wesley19097@gmail.com>
2019-06-12 15:04:47 +0200

    Change spacer minimum size

--- resources/js/components/Spacer.js
+++ resources/js/components/Spacer.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Spacer = ({ size = 1 }) => <div style={{ flex: size }} />
+const Spacer = ({ size = 1 }) => <div style={{ flex: `${size} ${size} 16px` }} />
 
 export default Spacer
