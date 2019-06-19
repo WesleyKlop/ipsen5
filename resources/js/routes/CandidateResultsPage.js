de4038d0f171b01376092b2c3aba780724aff180
Sergi Philipsen <philipsen.sergi@gmail.com>
2019-06-19 12:11:31 +0200

    Fix memmory leak on the results container

--- resources/js/routes/CandidateResultsPage.js
+++ resources/js/routes/CandidateResultsPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardHeader from '../components/card/CardHeader'
 import Card from '../components/card/Card'
 import ApiClient from '../ApiClient'
@@ -6,51 +6,41 @@ import Spacer from '../components/Spacer'
 import LinkButton from '../components/LinkButton'
 import ResultCard from '../components/ResultCard'
 
-class CandidateResultsPage extends React.Component {
-  state = {
-    results: [],
-  }
+const CandidateResultsPage = () => {
+  const [results, setResults] = useState([])
 
-  render() {
-    return (
-      <>
-        <Spacer />
-        <Card style={{ background: 'transparent', boxShadow: 'none' }}>
-          <CardHeader>
-            <h1>Resultaten</h1>
-          </CardHeader>
-          {this.state.results.map(result => (
-            <div key={result.id}>
-              <span>{result.name}</span>
-              {result.propositions.map(proposition => (
-                <ResultCard
-                  key={proposition.id}
-                  proposition={proposition.proposition}
-                  answer={proposition.answers[0].answer}
-                />
-              ))}
-            </div>
-          ))}
-        </Card>
-        <LinkButton to={'/email'}>Resultaten E-mailen</LinkButton>
-        <br />
-        <LinkButton to={'/profile'}>Profiel aanpassen</LinkButton>
-        <Spacer />
-      </>
-    )
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     ApiClient.request('answer')
-      .then(result => this.setResults(result))
+      .then(result => setResults(result))
       .catch(error => console.error(error))
-  }
+  }, [results])
 
-  setResults = results => {
-    this.setState({
-      results,
-    })
-  }
+  return (
+    <>
+      <Spacer />
+      <Card style={{ background: 'transparent', boxShadow: 'none' }}>
+        <CardHeader>
+          <h1>Resultaten</h1>
+        </CardHeader>
+        {results.map(result => (
+          <div key={result.id}>
+            <span>{result.name}</span>
+            {result.propositions.map(proposition => (
+              <ResultCard
+                key={proposition.id}
+                proposition={proposition.proposition}
+                answer={proposition.answers[0].answer}
+              />
+            ))}
+          </div>
+        ))}
+      </Card>
+      <LinkButton to={'/email'}>Resultaten E-mailen</LinkButton>
+      <br />
+      <LinkButton to={'/profile'}>Profiel aanpassen</LinkButton>
+      <Spacer />
+    </>
+  )
 }
 
 export default CandidateResultsPage
