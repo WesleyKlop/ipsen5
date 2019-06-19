1c6122b7dcdf29d9232cc71f29447d32e5312682
Sergi Philipsen <philipsen.sergi@gmail.com>
2019-06-19 10:54:13 +0200

    Add results container which chooses the result page that has to be shown (based on user type)

--- resources/js/containers/App.js
+++ resources/js/containers/App.js
@@ -1,26 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import VoterMainPage from '../routes/VoterMainPage'
 import InfoPage from '../routes/InfoPage'
 import PropositionPage from '../routes/PropositionPage'
 import FeedbackPage from '../routes/FeedbackPage'
-import VoterResultsPage from '../routes/VoterResultsPage'
 import EmailPage from '../routes/EmailPage'
 import Header from '../components/Header'
 import CandidateMainPage from '../routes/CandidateMainPage'
 import PageNotFoundPage from '../routes/PageNotFoundPage'
 import PrivateRoute from '../components/PrivateRoute'
 import CandidateProfilePage from '../routes/CandidateProfilePage'
-import CandidateResultsPage from '../routes/CandidateResultsPage'
-import Auth from '../Auth'
+import Results from './Results'
 
 const App = () => {
-  const [role, setRole] = useState(null)
-
-  useEffect(() => {
-    setRole(Auth.getRole())
-  }, [])
-
   return (
     <>
       <Header />
@@ -33,12 +25,7 @@ const App = () => {
             component={PropositionPage}
           />
           <PrivateRoute path="/feedback" component={FeedbackPage} />
-          <PrivateRoute
-            path="/results"
-            component={
-              role === 'voter' ? VoterResultsPage : CandidateResultsPage
-            }
-          />
+          <PrivateRoute path="/results" component={Results} />
           <PrivateRoute path="/email" component={EmailPage} />
           <Route path="/candidate/:loginCode" component={CandidateMainPage} />
           <Route path="/" exact component={VoterMainPage} />
