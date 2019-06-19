9bb3df853c78264c6d5aaedb9686be16cdb973ff
TomDeJong01 <dejongtom01@gmail.com>
2019-06-19 21:29:28 +0200

    change url to candidate/url ipv /url

--- resources/views/emails/emailCandidateRequest.blade.php
+++ resources/views/emails/emailCandidateRequest.blade.php
@@ -7,8 +7,8 @@
 
         Hieronder staat een link naar de stellingen waar we graag uw antwoorden op hebben:<br>
 
-        {{url($candidate->url)}}<br><br>
-
+        {{url('candidate'.$candidate->url)}}<br><br>
+        
         We hopen dat je het leuk vond om de Stem!App in te vullen, en zien je graag terug bij een volgende peiling!<br><br>
 
         Met vriendelijke groet,<br>
